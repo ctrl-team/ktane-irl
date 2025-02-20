d63b15864a9884aaa9b98f5e271bf7df5bea1402
Nimplex <39964594+Nimplex@users.noreply.github.com>
2025-02-20 16:16:37 +0100

    Add module to bus_controller.h, fix typo

--- src/main_timer_board/bus_controller.h
+++ src/main_timer_board/bus_controller.h
@@ -13,11 +13,12 @@ enum Module_state {
   SOLVED = 0x03,
   PAUSED = 0x04,
   NOT_STARTED = 0x05,
-  STATE_UNKOWN = 0xF
+  STATE_UNKNOWN = 0xF
 };
 
 enum Module_type {
   DEBUG_MODULE = 0x01,
+  BUTTON_MODULE = 0x2,
   MODULE_UNKNOWN = 0xF
 };
 
